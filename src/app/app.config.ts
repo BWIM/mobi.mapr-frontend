e592896e7b0e6e3e7f13e73f6ef82297d213878d
Pfennig <hello@pfennig.tech>
2025-02-13 18:22:06 +0100

    Add multilang concept

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideZoneChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import { provideRouter } from '@angular/router';
 import Material from '@primeng/themes/material';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { AuthInterceptor } from './auth/auth.interceptor';
-
 import { routes } from './app.routes';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -30,6 +38,17 @@ export const appConfig: ApplicationConfig = {
     }),
     provideHttpClient(
       withInterceptors([AuthInterceptor])
+    ),
+    importProvidersFrom(
+        HttpClientModule,
+        TranslateModule.forRoot({
+            defaultLanguage: 'de',
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        })
     )
   ]
 };
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { Project } from './project.interface';
+import { ProjectsService } from './projects.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-projects',
-  imports: [],
-  templateUrl: './projects.component.html',
-  styleUrl: './projects.component.css'
+    selector: 'app-projects',
+    templateUrl: './projects.component.html',
+    styleUrls: ['./projects.component.css'],
+    standalone: true,
+    imports: [CommonModule, TranslateModule]
 })
 export class ProjectsComponent {
+    projects: Project[] = [];
 
+    constructor(
+        private projectsService: ProjectsService
+    ) {}
+
+    ngOnInit() {
+        // Hier später die Projekte laden
+    }
 }
