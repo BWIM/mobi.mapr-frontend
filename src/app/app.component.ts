cd681162a0b1deb48bbc4e8ffda2680ee631d5ca
Pfennig <hello@pfennig.tech>
2025-02-13 13:05:58 +0100

    add header and sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenubarModule } from 'primeng/menubar';
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, MenubarModule, SidebarModule, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'mobi.mapr';
+  sidebarVisible: boolean = false;
+  items: MenuItem[] = [
+    {
+      label: 'Home',
+      icon: 'pi pi-home'
+    },
+    {
+      label: 'Menü',
+      icon: 'pi pi-bars',
+      command: () => this.sidebarVisible = true
+    }
+  ];
 }
