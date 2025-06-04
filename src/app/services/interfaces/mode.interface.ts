b13cdae77c12803e38c0cf3e65136cbcb7e22fb7
Pfennig <hello@pfennig.tech>
2025-06-04 16:15:37 +0200

    allow profiles instead of modes

--- src/app/services/interfaces/mode.interface.ts
+++ src/app/services/interfaces/mode.interface.ts
@@ -1,5 +1,12 @@
+export interface Profile {
+    id: number;
+    name: string;
+    display_name: string;
+}
+
 export interface Mode {
     id: number;
     name: string;
     display_name: string;
+    profiles: Profile[];
 } 
