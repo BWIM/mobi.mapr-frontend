1b27c552e56b1e7838b4cc2e5016e0cba0b0e1f8
Pfennig <hello@pfennig.tech>
2025-10-21 15:05:26 +0200

    Allow to group personas

--- src/app/services/personas.service.ts
+++ src/app/services/personas.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
+import { Observable, map, BehaviorSubject, of } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { Persona } from './interfaces/persona.interface';
 
@@ -16,6 +16,9 @@ interface PaginatedResponse<T> {
 })
 export class PersonasService {
   private apiUrl = environment.apiUrl;
+  private allPersonas: Persona[] = [];
+  private personasLoaded = false;
+  private personasSubject = new BehaviorSubject<Persona[]>([]);
 
   constructor(private http: HttpClient) { }
 
@@ -27,11 +30,29 @@ export class PersonasService {
     return this.http.get<PaginatedResponse<Persona>>(`${this.apiUrl}/personas/`, { params });
   }
 
-  getGroupedPersonas(): Observable<Persona[]> {
+  private loadAllPersonas(): Observable<Persona[]> {
+    if (this.personasLoaded) {
+      return of(this.allPersonas);
+    }
+
     return this.getPersonas().pipe(
       map(response => {
-        return response.results.sort((a, b) => a.name.localeCompare(b.name));
+        this.allPersonas = response.results.sort((a, b) =>
+          (a.display_name || a.name).localeCompare(b.display_name || b.name)
+        );
+        this.personasLoaded = true;
+        this.personasSubject.next(this.allPersonas);
+        return this.allPersonas;
       })
     );
+  }
+
+
+  getAllPersonas(): Observable<Persona[]> {
+    return this.loadAllPersonas();
+  }
+
+  getCachedPersonas(): Persona[] {
+    return this.allPersonas;
   }
 } 
