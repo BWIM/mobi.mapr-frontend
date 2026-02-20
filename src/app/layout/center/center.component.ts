1a983571d9574eb1f3c1dccb805764625ffe283d
pfennig <hello@pfennig.tech>
2026-02-20 14:19:45 +0100

    add legend

--- src/app/layout/center/center.component.ts
+++ src/app/layout/center/center.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit, OnDestroy, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit, ViewChild, ElementRef, inject, TemplateRef } from '@angular/core';
 import { Subscription, firstValueFrom } from 'rxjs';
 import { Map, NavigationControl, FullscreenControl } from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import { MapService } from '../../services/map.service';
 import MinimapControl from "maplibregl-minimap";
 import { SharedModule } from '../../shared/shared.module';
+import { FilterConfigService } from '../../services/filter-config.service';
+import { MatDialog } from '@angular/material/dialog';
+import { InfoDialogComponent } from '../../shared/info-overlay/info-dialog.component';
 
 
 @Component({
@@ -15,16 +18,71 @@ import { SharedModule } from '../../shared/shared.module';
 })
 export class CenterComponent implements OnInit, OnDestroy, AfterViewInit {
   @ViewChild('mapContainer') mapContainer?: ElementRef;
+  @ViewChild('legendContentTemplate') legendContentTemplate!: TemplateRef<any>;
   private map?: Map;
   private mapStyleSubscription?: Subscription;
   mapStyle: any;
   zoom: number = 7;
   center: [number, number] = [9.2156505, 49.320099];
+  private filterConfigService = inject(FilterConfigService);
+  private dialog = inject(MatDialog);
 
   constructor(private mapService: MapService) {}
 
   get isMapLoading() {
     return this.mapService.isMapLoading;
+  }
+
+  get selectedBewertung() {
+    return this.filterConfigService.selectedBewertung;
+  }
+
+  get isQualityMode() {
+    return this.selectedBewertung() === 'qualitaet';
+  }
+
+
+  // Quality (index) colors - A through F
+  qualityColors = [
+    { letter: 'A', color: 'rgba(50, 97, 45, 0.7)' },
+    { letter: 'B', color: 'rgba(60, 176, 67, 0.7)' },
+    { letter: 'C', color: 'rgba(238, 210, 2, 0.7)' },
+    { letter: 'D', color: 'rgba(237, 112, 20, 0.7)' },
+    { letter: 'E', color: 'rgba(194, 24, 7, 0.7)' },
+    { letter: 'F', color: 'rgba(150, 86, 162, 0.7)' }
+  ];
+
+  // Time (score) colors - gradient stops
+  timeColors = [
+    { value: '< 10', color: 'rgb(181, 212, 233)' },
+    { value: '10', color: 'rgb(147, 195, 224)' },
+    { value: '20', color: 'rgb(109, 173, 213)' },
+    { value: '30', color: 'rgb(75, 151, 201)' },
+    { value: '40', color: 'rgb(48, 126, 188)' },
+    { value: '50', color: 'rgb(24, 100, 170)' },
+    { value: '60+', color: 'rgb(24, 100, 170)' }
+  ];
+
+  getTimeGradient(): string {
+    return `linear-gradient(to right, 
+      rgb(181, 212, 233) 0%, 
+      rgb(147, 195, 224) 16.67%, 
+      rgb(109, 173, 213) 33.33%, 
+      rgb(75, 151, 201) 50%, 
+      rgb(48, 126, 188) 66.67%, 
+      rgb(24, 100, 170) 83.33%, 
+      rgb(24, 100, 170) 100%)`;
+  }
+
+  openLegendDialog(): void {
+    this.dialog.open(InfoDialogComponent, {
+      width: '80vw',
+      height: '80vh',
+      maxWidth: '80vw',
+      maxHeight: '80vh',
+      panelClass: 'info-dialog-panel',
+      data: { content: this.legendContentTemplate }
+    });
   }
 
   async ngOnInit() {
